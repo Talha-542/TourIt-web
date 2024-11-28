9c90904ceb4d7cfd74122f056195118b9e91d7c5
Talha-542 <ta662561@gmail.com>
2024-11-28 20:34:12 +0500

    project setup

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CreateTrip from './create-trip';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
