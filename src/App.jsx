9b8117552df98d18ee379e5aa71bb92ca9a0aba2
Talha-542 <ta662561@gmail.com>
2024-11-29 22:34:53 +0500

    Intro Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Hero from './components/ui/custom/Hero'
+import Intro from './components/ui/custom/Intro/Intro';
 
 function App() {
   return (
-   <Hero/>
+    <>
+    <Hero/>
+    <Intro/>
+    </>
   );
 }
 
