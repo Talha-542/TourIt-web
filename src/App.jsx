ec114029f133dd1cf23f4f9f2244ed61e6b9bd5b
Talha-542 <ta662561@gmail.com>
2024-12-13 12:21:26 +0500

    Dynamic Title and Favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import './App.css'
-import Hero from './components/ui/custom/Hero'
+import  { useEffect } from 'react';
+import './App.css';
+import Hero from './components/ui/custom/Hero';
 import Intro from './components/ui/custom/Intro/Intro';
 
-function App() {
+
+
+const App = () => {
+  useEffect(() => {
+    document.title = "TourIt";
+
+    const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
+    favicon.rel = "icon";
+    favicon.href = "/favicon.ico";
+    document.head.appendChild(favicon);
+  }, []);
+
   return (
     <>
-    <Hero/>
-    <Intro/>
+      <Hero />
+      <Intro />
     </>
   );
-}
+};
 
 export default App;
-
